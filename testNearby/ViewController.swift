4459adabb25217fe7ec21f83bb591ae8abbfc150
SahilDhawan <sahildhawan2011@gmail.com>
2018-01-29 13:16:26 +0530

    Initial Commit

---
+++ testNearby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testNearby
+//
+//  Created by Sahil Dhawan on 29/01/18.
+//  Copyright © 2018 Sahil Dhawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
